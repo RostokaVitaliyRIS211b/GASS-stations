73af6b53d4dac187ee423d2c2e8042076d9eb0a8
Виталий Ростока <rostoka.vit@yandex.ru>
2022-08-05 20:05:34 +0500

    Добавьте файлы проекта.

---
+++ Main.cs
@@ -0,0 +1,34 @@
+﻿using sus = System;
+using s = SFML.System;
+using g = SFML.Graphics;
+using w = SFML.Window;
+namespace Main
+{
+    class ProgramMain
+    {
+        public static int Main()
+        {
+            MainWindow Game= new MainWindow();
+            Game.GameLoop();
+            return 0;
+        }
+    }
+    class MainWindow
+    {
+        int width_screen = 700, height_screen = 700;
+        g.RenderWindow Window;
+        public MainWindow()
+        {
+            Window = new g.RenderWindow(new w.VideoMode((uint)width_screen,(uint)height_screen),"KING OF GAS STATIONS");
+        }
+        public void GameLoop()
+        {
+            while(Window.IsOpen)
+            {
+                Window.Clear(g.Color.White);
+                Window.DispatchEvents();
+                Window.Display();
+            }
+        }
+    }
+}
