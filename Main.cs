c42ccaa3a5042365f26c4438cd8c656ce724587b
Виталий Ростока <rostoka.vit@yandex.ru>
2022-08-08 15:17:50 +0500

    fixed init and pos

--- Main.cs
+++ Main.cs
@@ -18,7 +18,7 @@ namespace Main
     class MainWindow
     {
         sus.Random rand = new sus.Random();
-        public int width_screen = 700, height_screen = 700,count_of_players = 1,size_of_colod=3,player=1;
+        public int width_screen = 1280, height_screen = 720,count_of_players = 1,size_of_colod=3,player=1;
         g.RenderWindow Window;
         scg.List<scg.List<c.Card>> colods = new scg.List<scg.List<c.Card>>();
         scg.List<c.Card> map = new scg.List<c.Card>();
@@ -31,6 +31,9 @@ namespace Main
         }
         public void GameLoop()
         {
+            initialize_colods();
+            foreach (scg.List<c.Card> list in colods)
+                set_coords_colod(list);
             while(Window.IsOpen)
             {
                 Window.Clear(g.Color.White);
@@ -51,24 +54,37 @@ namespace Main
                 for (int j = 0; j < size_of_colod; ++j)
                 {
                     string name = jpgs[rand.Next(jpgs.Length)];
-                    if(name.Contains("gas"))
+                    if(name.Contains("road"))
                     {
                         c.Doroga doroga = new c.Doroga{ player = i+1, sprite = new g.Sprite(new g.Texture(new g.Image(name)))};
+                        doroga.sprite.Origin = new s.Vector2f(50, 50);
                         coloda.Add(doroga);
                     }
                     else
                     {
                         c.Trafic trafic = new c.Trafic { player = i + 1, sprite = new g.Sprite(new g.Texture(new g.Image(name))) };
+                        trafic.sprite.Origin = new s.Vector2f(50, 50);
                         coloda.Add(trafic);
                     }
                 }
-                for(int j = 4*i; j<4*i+4;++j)
+                for(int j = 4*i; j<4*i+3;++j)
                 {
                     string name = gasstations[j];
                     c.GasStation gas = new c.GasStation { player = i + 1, sprite = new g.Sprite(new g.Texture(new g.Image(name))) };
+                    gas.sprite.Origin = new s.Vector2f(50, 50);
+                    coloda.Add(gas);
                 }
+                colods.Add(coloda);
 
             }
+        }
+        void set_coords_colod(scg.List<c.Card> coloda)
+        {
+            float x = width_screen/2 - 110 * coloda.Count/2,y=height_screen-75;
+            for(int i=0;i<coloda.Count;++i)
+                coloda[i].sprite.Position = new s.Vector2f(x + 110f * i, y);
+            
+
         }
         void Window_MouseMoved(object sender,w.MouseMoveEventArgs e)
         {
