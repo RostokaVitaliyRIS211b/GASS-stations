438e98f6ff6059c33ead27fe3054511b92ac7d75
Виталий Ростока <rostoka.vit@yandex.ru>
2022-08-08 14:20:41 +0500

    add init

--- Card.cs
+++ Card.cs
@@ -4,10 +4,11 @@ using g = SFML.Graphics;
 using w = SFML.Window;
 namespace Cards
 {
-    internal class Card:g.Drawable
+    internal abstract class Card : g.Drawable
     {
-        g.Sprite sprite;
-        int player;
+        
+        public g.Sprite sprite { get; set; }
+        public int player { get; set; }
         public Card()
         {
             sprite = new g.Sprite();
@@ -18,6 +19,7 @@ namespace Cards
             sprite = new g.Sprite(card.sprite.Texture);
             player = card.player;
         }
+        abstract public void Parse(string name);
         public void Draw(g.RenderTarget target, g.RenderStates states)
         {
             target.Draw(sprite, states);
@@ -25,15 +27,44 @@ namespace Cards
     }
     internal class Doroga : Card
     {
-
+        bool way1, way2, way3, way4;
+        public Doroga() : base() 
+        {
+            way1 = false;
+            way2 = false;
+            way3 = false;
+            way4 = false;
+        }
+        public override void Parse(string name)
+        {
+            way1 = name.Contains("1");
+            way2 = name.Contains("2");
+            way3 = name.Contains("3");
+            way4 = name.Contains("4");
+        }
     }
     internal class GasStation : Card
     {
+        int level;
+        public GasStation() : base()
+        {
+            level = 0;
+        }
+        public override void Parse(string name)
+        {
 
+        }
     }
     internal class Trafic : Card
     {
+        public Trafic() : base()
+        {
 
+        }
+        public override void Parse(string name)
+        {
+
+        }
     }
 }
 
--- Main.cs
+++ Main.cs
@@ -2,34 +2,78 @@
 using s = SFML.System;
 using g = SFML.Graphics;
 using w = SFML.Window;
-using gas = Cards;
+using c = Cards;
+using scg = System.Collections.Generic;
 namespace Main
 {
     class ProgramMain
     {
         public static int Main()
         {
-            MainWindow Game= new MainWindow();
+            MainWindow Game = new MainWindow();
             Game.GameLoop();
             return 0;
         }
     }
     class MainWindow
     {
-        int width_screen = 700, height_screen = 700;
+        sus.Random rand = new sus.Random();
+        public int width_screen = 700, height_screen = 700,count_of_players = 1,size_of_colod=3,player=1;
         g.RenderWindow Window;
+        scg.List<scg.List<c.Card>> colods = new scg.List<scg.List<c.Card>>();
+        scg.List<c.Card> map = new scg.List<c.Card>();
+        string [] jpgs = { "images/road_1_2_3_4.png", "images/road_1.png", "images/road_2.png", "images/road_3.png", "images/road_4.png", "images/road_1_2_3.png", "images/road_4_1_2.png", "images/road_1_2.png", "images/road_1_3.png", "images/road_2_3_4.png", "images/road_2_3.png", "images/road_2_4.png", "images/road_3_4.png", "images/road_4_1.png", "images/road_3_4_1.png", "images/car_stream.png" };
+        string [] gasstations = { "images/gas_station_1_red.png", "images/gas_station_2_red.png", "images/gas_station_3_red.png", "images/gas_station_1_blue.png", "images/gas_station_2_blue.png", "images/gas_station_3_blue.png", "images/gas_station_1_green.png", "images/gas_station_2_green.png", "images/gas_station_3_green.png", "images/gas_station_1_orange.png", "images/gas_station_2_orange.png", "images/gas_station_3_orange.png" };
         public MainWindow()
         {
             Window = new g.RenderWindow(new w.VideoMode((uint)width_screen,(uint)height_screen),"KING OF GAS STATIONS");
+            Window.MouseMoved += Window_MouseMoved;
         }
         public void GameLoop()
         {
             while(Window.IsOpen)
             {
                 Window.Clear(g.Color.White);
+                foreach (c.Card card in colods[player - 1])
+                    Window.Draw(card);
+                foreach (c.Card card in map)
+                    Window.Draw(card);
                 Window.DispatchEvents();
                 Window.Display();
             }
         }
+
+        void initialize_colods()
+        {
+            for(int i=0;i<count_of_players;++i)
+            {
+                scg.List<c.Card> coloda = new scg.List<c.Card>();
+                for (int j = 0; j < size_of_colod; ++j)
+                {
+                    string name = jpgs[rand.Next(jpgs.Length)];
+                    if(name.Contains("gas"))
+                    {
+                        c.Doroga doroga = new c.Doroga{ player = i+1, sprite = new g.Sprite(new g.Texture(new g.Image(name)))};
+                        coloda.Add(doroga);
+                    }
+                    else
+                    {
+                        c.Trafic trafic = new c.Trafic { player = i + 1, sprite = new g.Sprite(new g.Texture(new g.Image(name))) };
+                        coloda.Add(trafic);
+                    }
+                }
+                for(int j = 4*i; j<4*i+4;++j)
+                {
+                    string name = gasstations[j];
+                    c.GasStation gas = new c.GasStation { player = i + 1, sprite = new g.Sprite(new g.Texture(new g.Image(name))) };
+                }
+
+            }
+        }
+        void Window_MouseMoved(object sender,w.MouseMoveEventArgs e)
+        {
+
+        }
+
     }
 }
